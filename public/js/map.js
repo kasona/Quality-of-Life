88cf5b0f880d43d458d2eec7eacdbd6686157c27
Kasi Onaga <kasi@Kasis-MacBook-Air.local>
2015-11-14 09:19:15 -1000

    Added map to website. Need to Add Francis's functions to /partials/map-fills.jade Map is not responsive ( its ok, focus on getting the colors to change ) Need to add dropdowns near the map for changing colors.

---
+++ public/js/map.js
@@ -0,0 +1,49 @@
+//Define Variables
+// var state = stateId;
+// var color = val; // ?
+
+// Load map into the container
+$("#container").load("Blank_US_Map.svg", function() {
+  // state abb + color in hex
+  // $("#"+ state).css("fill", color);
+  $("#CA").css("fill", "pink");
+
+});
+
+//job cat states
+// var stateId = stateies.map(function(obj) {
+//   var var = obj.house;
+// })
+
+
+// //job cat salaries
+// var salaryCss = salaries.map(function(obj) {
+//   var val = obj.salary;
+//   return val <= 30000 ? '#E0EEEE' :
+//     (val <= 60000 ? '#D1EEEE' :
+//     (val <= 90000 ? '#96CDCD' :
+//     (val <= 120000 ? '#388E8E' :
+//     (val <= 150000 ? '#008080' :
+//     'white'))));
+// });
+
+
+// var salaries = [ sample data here ]
+
+
+/**
+ * Job Cat
+ * 1. Salary
+ * 2. Counts
+ * 3. Popularity index
+ *
+ * Homes
+ * 1. Median Price
+ * 2. Counts
+ * 3. Popularity Index
+ *
+ * Rentals
+ * 1. Median Price
+ * 2. Counts
+ * 3. Popularity Index
+ */
